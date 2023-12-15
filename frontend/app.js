7a3110856db716543e9b41a9940261f9104ba208
Abdul Manan <abdulmananshah02@gmail.com>
2023-12-15 13:40:38 +0500

    testing

--- frontend/app.js
+++ frontend/app.js
@@ -18,6 +18,7 @@ app.get('/hello', (req, res) => {
   res.send('Hello world\n');
 });
 
+
 // Define your API endpoints and routes here
 app.get('/api/doctors', async (req, res) => {
   let url  = process.env.DOCTORS_SERVICE_URL;
